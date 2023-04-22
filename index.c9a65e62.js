2a6e24cb6788e59ec1384d3be678d3fa4bd3ec6d
JacekPasierb <113291374+JacekPasierb@users.noreply.github.com>
2023-04-22 21:36:59 +0000

    Deploying to gh-pages from @ JacekPasierb/filmonetka@6ea0fd96c3d2b76bc1881df5bd3a6a9ee36b5751 🚀

---
+++ index.c9a65e62.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.c9a65e62.js.map
