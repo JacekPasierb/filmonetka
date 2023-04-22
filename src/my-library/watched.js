f02c4392af4736f64fa499451eb850bb3a471bca
Jacek Pasierb <jack_byk@o2.pl>
2023-04-22 15:51:25 +0200

    Poprawiono importy

--- src/my-library/watched.js
+++ src/my-library/watched.js
@@ -1,13 +1,12 @@
-import { getWatchedLocalStorage } from "./local-storage";
-import { watched } from "./local-storage";
+import localStorage from "./local-storage";
 import { renderMovieCard } from "../js/renderMovieCards";
 import Trending from "../js/trendingMovie";
-import { movieGenres } from "../js/genres";
+import Genres from "../js/genres";
 
 const markButton = document.querySelector(".button-mark");
 const movieCard = document.querySelector(".movie-container__card");
-markButton.addEventListener("click", markFunction)
+markButton.addEventListener("click", markFunction);
 function markFunction() {
-    const innerHTML = '<span status = "watched"></span>'
-movieCard.prepend(innerHTML);
-}    
+	const innerHTML = '<span status = "watched"></span>';
+	movieCard.prepend(innerHTML);
+}
