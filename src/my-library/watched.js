0ffeb9d49efa5e5caf78719936f61132732ee03d
Jacek Pasierb <jack_byk@o2.pl>
2023-05-01 11:08:01 +0200

    usunieto martwe pliki.js

--- src/my-library/watched.js
+++
@@ -1,9 +0,0 @@
-import { qs } from "../js/tools";
-
-const markButton = qs(".button-mark");
-const movieCard = qs(".movie-container__card");
-markButton.addEventListener("click", markFunction);
-const markFunction = () => {
-	const innerHTML = '<span status = "watched"></span>';
-	movieCard.prepend(innerHTML);
-};
